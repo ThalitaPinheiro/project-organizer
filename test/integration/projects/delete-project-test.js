cd99df72381dd1aab2c659d8be5ef702b3af67a3
ThalitaPinheiro <thalitanpg@gmail.com>
2016-08-13 19:42:24 -0300

    delete project

--- test/integration/projects/delete-project-test.js
+++ test/integration/projects/delete-project-test.js
@@ -5,43 +5,40 @@ var assert = require('chai').assert;
 
 var BASE_LIB = ('../../../lib/');
 var db = require(BASE_LIB  + 'commons/database');
+var projectModel = require(BASE_LIB + 'source/models').project
 var app = require(BASE_LIB + 'application');
-var projectPayload = {
-  name: 'project test'
-};
+var name = 'project to delete'
 
-describe('POST projects integration tests - ', function() {
+describe('DELETE projects integration tests - ', function() {
+  beforeEach(function(done) {
+    var now = new Date();
+    var project = {
+      name : name,
+      createdAt: now,
+      updatedAt: now
+    };
+    projectModel.create(project, done);
+  });
+
   afterEach(function(done) {
     db.dropCollections('projects', done);
   });
 
-  it('Should return 201. Project created', function(done) {
+  it('Should return 200. Project deleted', function(done) {
     request(app)
-    .post('/projects')
-    .send(projectPayload)
+    .delete('/projects/' + name)
+    .send()
     .end(function(err, res) {
       assert.isNull(err);
-      assert.equal(201, res.statusCode);
+      assert.equal(200, res.statusCode);
       done();
     });
   });
 
   it('Should return 400 (bad request) cause project payload has no name', function(done) {
     request(app)
-    .post('/projects')
+    .delete('/projects/nonExistentProject')
     .send({})
-    .end(function(err, res) {
-      assert.isNull(err);
-      assert.equal(400, res.statusCode);
-      done();
-    });
-  });
-
-  it('Should return 400 (bad request) cause project name isn\'t strig', function(done) {
-    var payload = { name: { teste: 'badRequest' } } ;
-    request(app)
-    .post('/projects')
-    .send(payload)
     .end(function(err, res) {
       assert.isNull(err);
       assert.equal(400, res.statusCode);
